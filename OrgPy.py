932c30a7bdd3f374501102c262546e8cf3a4cb9a
Philipp Paulweber <p.paulweber@gmail.com>
2014-07-08 01:56:48 +0200

    Improvement for the org-mode parser
    * added new classes
    * fixed some parse bugs
    * improved recognition patterns

--- OrgPy.py
+++ OrgPy.py
@@ -14,22 +14,56 @@ sys.path.append( "../verbose/python" )
 
 from Verbose import *
 
+class OrgModeObject :
+    pass
+
+class Heading( OrgModeObject ) :
+    pass
+
+class Paragraph( OrgModeObject ) :
+    pass
+
+class Table( OrgModeObject ) :
+    pass
+
+class List( OrgModeObject ) :
+    pass
+
+class lal( OrgModeObject ) :
+    pass
+
+
+
+
 class OrgPy :
+    
+    
+    
     def __init__( self, filename, **option ) :
         
         if not os.path.exists( filename ) :
             assert( 0 )
         
+        self._filename = filename;
+        self._file = []
+        
+        self._structure = []
+        
         orgfile = open( filename, "r" )    
         
-        self._file = []
+        
         
         heading = re.compile( "^\*+ " )
         
+        
+        table   = re.compile( "\|-*-\||\|.*\|" )
+        itemize = re.compile( "-\s|\+\s" )
+        
         italic  = re.compile( "/\S+/" )
         bold    = re.compile( "\*\S+\*" )
+        link    = re.compile( "(http://\S+)" )
         
-        ignore = re.compile( "[\n]|//|\*\*" )
+        ignore  = re.compile( "[\n]|\*\*" )
         
         cnt = 0
         for line in orgfile.readlines() :
@@ -37,6 +71,7 @@ class OrgPy :
             
             h = heading.findall( line )
             if len( h ) > 0 :
+                printf( "%{magenta:%s%}", len(h[0])-1 )
                 line = line[ len(h[0]) : ]
             
             line = ignore.sub( "", line )
@@ -46,6 +81,15 @@ class OrgPy :
             
             for i in bold.finditer( line ) :
                 printf( "%{yellow:%s%}", i.span() )
+            
+            for i in table.finditer( line ) :
+                printf( "%{yellow:%s%}", i.span() )
+            
+            for i in itemize.finditer( line ) :
+                printf( "%{yellow:%s%}", i.span() )
+                
+            for i in link.finditer( line ) :
+                printf( "%{blue:%s%}", i.span() )
             
             printf( "%-50s%-3i: h=%s", line, cnt, h )
             cnt = cnt + 1
