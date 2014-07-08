8bfb2e1ce65310407ecb2240bd1f299c4adcc2bd
Philipp Paulweber <p.paulweber@gmail.com>
2014-07-08 20:31:34 +0200

    Added org-mode class representations
    * started to represent all org-mode features
      via a base class OrgModeContent
    * parser/lexer will create a AST which is based
      on plain python lists

--- OrgPy.py
+++ OrgPy.py
@@ -14,23 +14,99 @@ sys.path.append( "../verbose/python" )
 
 from Verbose import *
 
-class OrgModeObject :
+class OrgModeOption( object ) :
     pass
+# end class
 
-class Heading( OrgModeObject ) :
-    pass
+class OrgModeContent :
+    def __init__( self, line = None ) :
+        self.__line    = line
+        self.__options = []
+        self.__content = []
+    # end def
+    
+    def append( self, content ) :
+        assert( isinstance( content, OrgModeContent ) )
+        self.__content.append( content )
+    # end def
+    
+    def add( self, option ) :
+        assert( isinstance( option, OrgModeOption ) )
+        self.__options.append( option )
+    # end def
+    
+    def dump( self, indent = 0 ) :
+        
+        ind = ""
+        for i in range( indent ) :
+            ind = "%s " % ind
+        
+        printf( "%s%s: '%s', %s", ind, self.__class__.__name__, self.__line, self.__options ) 
+        
+        for c in self.__content :
+            c.dump( indent+1 )
+        
+    # end def
+# end class
 
-class Paragraph( OrgModeObject ) :
-    pass
+class Heading( OrgModeContent ) :
+    def __init__( self, line, options, depth ) :
+        OrgModeContent.__init__( self, line )
+        self.depth = depth
+    # end def
+# end class
 
-class Table( OrgModeObject ) :
-    pass
+class Paragraph( OrgModeContent ) :
+    def __init__( self ) :
+        OrgModeContent.__init__( self )
+    # end def
+# end class
 
-class List( OrgModeObject ) :
-    pass
+#==============================================================================
+# TABLE
+#==============================================================================
 
-class lal( OrgModeObject ) :
-    pass
+class Table( OrgModeContent ) :
+    def __init__( self ) :
+        OrgModeContent.__init__( self )
+    # end def
+
+    def append( self, content ) :
+        assert( issubclass( content, TableRow ) )
+        self.__content.append( content )
+    # end def
+# end class
+
+class TableRow( OrgModeContent ) :
+    def __init__( self, line = None ) :
+        OrgModeContent.__init__( self, line )
+    # end def
+# end class
+
+class TableLine( TableRow ) :
+    def __init__( self ) :
+        TableRow.__init__( self )
+    # end def
+# end class
+
+#==============================================================================
+# LIST
+#==============================================================================
+        
+class List( OrgModeContent ) :
+    def __init__( self, line, options, depth ) :
+        OrgModeContent.__init__( self, line )
+        self.depth = depth
+    # end def
+# end class
+
+class lal( OrgModeContent ) :
+    def __init__( self, line, options, depth ) :
+        OrgModeContent.__init__( self, line )
+        self.depth = depth
+    # end def
+# end class
+
 
 
 
@@ -57,6 +133,8 @@ class OrgPy :
         
         
         table   = re.compile( "\|-*-\||\|.*\|" )
+        column  = re.compile( "\||\+" )
+        
         itemize = re.compile( "-\s|\+\s" )
         
         italic  = re.compile( "/\S+/" )
@@ -65,16 +143,24 @@ class OrgPy :
         
         ignore  = re.compile( "[\n]|\*\*" )
         
+        content = OrgModeContent( None )
+        
+        stack = [ content ]
+        
         cnt = 0
         for line in orgfile.readlines() :
             self._file.append( line )
+            
+            line = ignore.sub( "", line )
             
             h = heading.findall( line )
             if len( h ) > 0 :
                 printf( "%{magenta:%s%}", len(h[0])-1 )
                 line = line[ len(h[0]) : ]
+                
+                content.append( Heading( line, [], len(h[0])-1 ) )
+                
             
-            line = ignore.sub( "", line )
             
             for i in italic.finditer( line ) :
                 printf( "%{green:%s%}", i.span() )
@@ -84,6 +170,8 @@ class OrgPy :
             
             for i in table.finditer( line ) :
                 printf( "%{yellow:%s%}", i.span() )
+                printf( "%{yellow:%s%}", line[ i.span()[0] : i.span()[1] ].split("|") )
+            #    printf( "%{yellow:%s%}", column.split( line[ i.span()[0] : i.span()[1] ]) )
             
             for i in itemize.finditer( line ) :
                 printf( "%{yellow:%s%}", i.span() )
@@ -95,6 +183,8 @@ class OrgPy :
             cnt = cnt + 1
         
         printf( "%s", len( self._file ) )
+
+        content.dump()
         
     # end def
 # end class
